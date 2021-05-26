64d7852ff8eb24a7bcc232bcf64393050702aec1
ameyjakate20001 <ameyjakate2001@gmail.com>
2021-05-26 13:35:16 -0700

    ready  for commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express() ;
+const port = 5000;
+const bp = require('body-parser')
+const qr = require('qrcode')
+
+app.set('view engine', "ejs");
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/scan' , (req, res) => {
+   const url = req.body.url;
+   if(url.length === 0)
+      res.send("empty Data !!")
+    
+      qr.toDataURL(url, (err, src) => {
+          if(err) res.send('error occured')
+          console.log(src);
+          res.render("scan", {src})
+      })
+})
+
+
+app.listen(port, () => {
+    console.log('server at 5000')
+})
