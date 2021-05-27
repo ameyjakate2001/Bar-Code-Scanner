32dce2faf37731ab5701e5224cd7c39249bfb43f
ameyjakate20001 <ameyjakate2001@gmail.com>
2021-05-26 17:55:01 -0700

    front end added

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { v1: uuidv1, v4: uuidv4 } = require("uuid");
 app.set('view engine', "ejs");
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.render('index')
